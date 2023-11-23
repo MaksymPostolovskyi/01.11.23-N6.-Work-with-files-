75d96e033b3c7a201fef67e80908d46ddb3afad1
MP <144225366+MaksymPostolovskyi@users.noreply.github.com>
2023-11-23 13:41:46 +0200

    import math1.py

--- 08.11.23/import math1.py
+++ 08.11.23/import math1.py
@@ -1,10 +1,20 @@
 import math
 
-file = open("input.txt", "r")
-b1 = float(file.read())
-a1 = math.atan(b1)
-y = a1/(b1**2 + math.sqrt(a1))
-print(f"b1 = {b1}\na1 = {a1}\ny = {y}")
-    
-f = open("output.txt", "w")
-f.write(f"a1 = {a1}\ny = {y}")
+def data(file_path='input.txt'):
+    with open(file_path, 'r') as file:
+        b1 = float(file.read())
+        a1 = math.atan(b1)
+        y = a1/(b1**2 + math.sqrt(a1))
+       
+
+    return b1, a1, y
+
+result = data()  
+
+result_str = f"b1: {result[0]}\n a1: {result[1]}\n y: {result[2]}"
+
+print(result_str)
+
+
+with open("output.txt", 'w') as f:
+    f.write(result_str)
